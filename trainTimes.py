e411f0c0a23a27c168ff76ccb5772b506d19fcb7
cristobers <chris@MacBook-Pro.localdomain>
2021-12-28 19:55:40 +0000

    fixed spelling mistake

--- trainTimes.py
+++ trainTimes.py
@@ -27,7 +27,7 @@ r = requests.get(url)
 
 print("Departs from",args.o , "\t", "Arrives At", args.d)
 for x in range(0, 5):
-    # this try and except was included because i am lazy and dont want to count the amount of journies that show up in the JSON response.
+    # this try and except was included because i am lazy and dont want to count the amount of journeys that show up in the JSON response.
     try:
         departureTime = str(r.json()["legs"][0]["legSolution"][x]["travelSegments"][0]["departureDateTime"])[11:]
         arrivalTime = str(r.json()["legs"][0]["legSolution"][x]["travelSegments"][0]["arrivalDateTime"])[11:]
