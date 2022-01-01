ade557f399930a735b46e04015e1f703623e8dd5
cristobers <chris@MacBook-Pro.localdomain>
2022-01-01 19:08:12 +0000

    working example of using TFWs API to pull the internal station names from user input

--- getStationNames.py
+++ getStationNames.py
@@ -2,4 +2,9 @@ import requests
 
 def GetStationNames(user_input):
 	r = requests.get('https://tfw.wales/api/silverrail-stations/find/{}'.format(user_input))
-	return r.json()
+	counter = 0
+	station_name = str(r.json()[counter]['name'])
+	if station_name != user_input:
+		counter += 1
+	else:
+		return str(r.json()[counter]['sr_code'])
