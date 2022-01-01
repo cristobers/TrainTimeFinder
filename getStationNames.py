0163c7b242be61d368bd3cfe54c24e0755a5738e
cristobers <chris@MacBook-Pro.localdomain>
2022-01-01 19:18:42 +0000

    added ValueError return message

--- getStationNames.py
+++ getStationNames.py
@@ -1,10 +1,18 @@
 import requests
 
 def GetStationNames(user_input):
-	r = requests.get('https://tfw.wales/api/silverrail-stations/find/{}'.format(user_input))
+	try:
+		r = requests.get('https://tfw.wales/api/silverrail-stations/find/{}'.format(user_input))
+		r.raise_for_status()
+	except requests.exceptions.RequestException as e:
+		raise SystemExit(e)
 	counter = 0
-	station_name = str(r.json()[counter]['name'])
-	if station_name != user_input:
-		counter += 1
-	else:
-		return str(r.json()[counter]['sr_code'])
+	try:
+		station_name = str(r.json()[counter]['name'])
+		
+		if station_name != user_input:
+			counter += 1
+		else:
+			return str(r.json()[counter]['sr_code'])
+	except ValueError:
+		print('value error, either you didnt spell the station correctly or you didnt type in the correct name of the station.')
