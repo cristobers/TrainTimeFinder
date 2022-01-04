35dbb25544978370c2066be0c2fab77fcad507f1
cristobers <chris@MacBook-Pro.localdomain>
2022-01-04 19:13:47 +0000

    clean up

--- getStationNamesAPI.py
+++ getStationNamesAPI.py
@@ -2,7 +2,7 @@ import requests
 
 def getStationNames(user_input):
     # maybe add try/except for http errors 
-    r = requests.get(f'https://tfw.wales/api/silverrail-stations/find/{user_input}').json()
+    r = requests.get(f"https://tfw.wales/api/silverrail-stations/find/{user_input}").json()
     for x in r:
-        if x['name'] == user_input:
-            return x['sr_code']
+        if x["name"] == user_input:
+            return x["sr_code"]
