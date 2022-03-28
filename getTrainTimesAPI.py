411e2a90c93d2596e35f95fd84fcbac3b23ffd5d
cristobers <chris@MacBook-Pro.localdomain>
2022-03-28 21:32:20 +0100

    added check for 200 status code

--- getTrainTimesAPI.py
+++ getTrainTimesAPI.py
@@ -2,5 +2,8 @@ import requests
 
 def getTrainTimes(origin, destination, today, time):
     # my hope is that eventually this will handle all of the time gathering, for now it gets requests from the API.
-    r = requests.get(f"https://tickets.trc.cymru/api/v1/silverrail/ticketsearch?token=Ir00ktJ11ZwvdSoNX79E&origin={origin}&destination={destination}&outboundDate={today}&outboundTime={time}&timeWindowInbound=departure&timeWindowOutbound=departure&ticketType=single&earlierSearch=false&adult=1&child=0&tfwRestricted=false&channelCode=WEB").json()["legs"][0]["legSolution"]
-    return r
+    r = requests.get(f"https://tickets.trc.cymru/api/v1/silverrail/ticketsearch?token=Ir00ktJ11ZwvdSoNX79E&origin={origin}&destination={destination}&outboundDate={today}&outboundTime={time}&timeWindowInbound=departure&timeWindowOutbound=departure&ticketType=single&earlierSearch=false&adult=1&child=0&tfwRestricted=false&channelCode=WEB")
+    if r.status_code != 200:
+        print("error with getting train times.") 
+    else:
+        return r.json()["legs"][0]["legSolution"]
