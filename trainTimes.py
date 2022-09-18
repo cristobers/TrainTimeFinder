7f909c638ef4e6b6a8b861e17689532ad0059db4
cris <72763707+cristobers@users.noreply.github.com>
2022-09-18 16:06:22 +0100

    added ability to enter a date and time using '-D or -date' and '-t' respectively

--- trainTimes.py
+++ trainTimes.py
@@ -7,15 +7,29 @@ from getStationNamesAPI import getStationNames
 from getTrainTimesAPI import getTrainTimes
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-o", "-O", help="Origin Station")
-parser.add_argument("-d", "-D", help="Destination Station")
+parser.add_argument("-o", help="Origin Station", metavar='\b')
+parser.add_argument("-d", help="Destination Station", metavar='\b')
+parser.add_argument("-t", help="Specific Time, giving times in the past throws an error ( no time travelling allowed )", metavar='\b')
+parser.add_argument("-date", "-D", help="Specific Date, months are formatted as such: 2022-09-18, months are zero-padded if they're < 10", metavar='\b')
 args = parser.parse_args()
 
 origin = args.o
 destination = args.d
+time = args.t
+day = args.date
 
-today = date.today().strftime('%Y-%m-%d')
-time = strftime("%H:%M", gmtime())
+if day is None:
+    today = date.today().strftime('%Y-%m-%d')
+else:
+    today = str(day)
+
+if time is None:
+    time = strftime("%H:%M", gmtime())
+else:
+    if len(str(time)) == 4 and str(time)[0] != '0':
+        time = '0' + str(time)
+    else:
+        time = str(time)
 
 print(args.o, "-->", args.d)
 for time in getTrainTimes(origin, destination, today, time):
